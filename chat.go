4251c5a68ae590201f2d8b938f4c39dba7a34ebe
Christian <3003html@gmail.com>
2022-07-20 10:07:56 -0500

    Handle connection function finished

--- chat.go
+++ chat.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"net"
 )
 
@@ -10,6 +12,8 @@ type client chan<- string
 var (
 	incomingClients = make(chan client)
 	leavingClients  = make(chan client)
+
+	// Global channel
 	messagesClients = make(chan string)
 )
 
@@ -21,6 +25,31 @@ var (
 // Function to handle the connection of clients with the server
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	messageChat := make(chan string)
 
+	// Messages from the client that has connected
+	messageChat := make(chan string)
+	go messageWrite(conn, messageChat)
+
+	// Remote Network Address
+	clientName := conn.RemoteAddr().String()
+
+	messageChat <- fmt.Sprintf("Welcome to the server, your name %s\n", clientName)
+
+	// Message for all clients
+	messagesClients <- fmt.Sprintf("New client is here, name %s\n", clientName)
+
+	incomingClients <- messageChat
+
+	inputMessage := bufio.NewScanner(conn)
+	for inputMessage.Scan() {
+		messagesClients <- fmt.Sprintf("%s: %s\n", clientName, inputMessage.Text())
+	}
+	leavingClients <- messageChat
+	messagesClients <- fmt.Sprintf("Client %s left the chat\n", clientName)
+}
+
+func messageWrite(conn net.Conn, messages <-chan string) {
+	for message := range messages {
+		fmt.Fprintln(conn, message)
+	}
 }
